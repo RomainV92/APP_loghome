f07433f523f372a5fa005a9ff7f57dcdbde984ab
Romain Valaix <romain.valaix@isep.fr>
2018-05-24 11:53:48 +0200

    Réglages include

--- controleur/Page_contact_us.php
+++ controleur/Page_contact_us.php
@@ -1,18 +1,10 @@
 <?php
-<<<<<<< HEAD
 include('../modele/bdd_access.php');
 $bdd=appel_bdd();
 
-include("modele/bdd_access_maison.php");
-include('vue/frequent/menu.php');
-include('vue/contact_us.php');
-include('vue/frequent/footer.php');
-=======
-session_start();
 include("../modele/bdd_access_maison.php");
 include('../vue/frequent/menu.php');
 include('../vue/contact_us.php');
 include('../vue/frequent/footer.php');
->>>>>>> e70da3b10195691b0e442d276843903af81b487a
 
 ?>
