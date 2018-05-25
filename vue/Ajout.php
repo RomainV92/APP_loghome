9a161f169898d769fbbb6859abcecde569a78b4b
Romain Valaix <romain.valaix@isep.fr>
2018-05-25 15:08:44 +0200

    Réglage page inscription

--- vue/Ajout.php
+++ vue/Ajout.php
@@ -6,18 +6,10 @@
     <link rel="stylesheet" href="../vue/inscription.css"/>
   </head>
   <body>
-<<<<<<< HEAD
     <div class="InBox">
       <img src="../images/image_Log.png" class="avatar">
       <h1> Inscription </h1>
       <form method="post" action=<?php echo $Direction; ?> />
-=======
-    <div class="wrapper">
-      <div class="InBox">
-        <img src="../images/image_Log.png" class="avatar">
-        <h1> Inscription </h1>
-        <form method="post" action=<?php echo $Direction; ?>>
->>>>>>> 6e94a131e901d29d654480375b8893b4b917ab13
           <p>Votre Prenom </p>
           <input id="Prenom" type="text" name="Prenom" id="Prenom" required/>
           <p>Votre Nom </p>
@@ -32,9 +24,5 @@
         <span class="Error"><?php echo $Error_message; ?></span>
       </div>
     </div>
-<<<<<<< HEAD
-=======
-
->>>>>>> 6e94a131e901d29d654480375b8893b4b917ab13
   </body>
 </html>
