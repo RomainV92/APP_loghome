34b4acc492813cd0a3be78e636c0daedc53bd32c
jonathan <phung.truong@isep.fr>
2018-05-25 16:14:22 +0200

    page aide

--- vue/Aide.php
+++ vue/Aide.php
@@ -10,8 +10,15 @@
   <body>
   
     <div class="wrapper">
-      <a href="../index.php?cible=faq'">Accéder à la F.A.Q</a></li> 
-      <a href="../index.php?cible=Page_contact_us'">Envoyer une requête/a></li> 
+      <div class="faq"
+        <a href="../index.php?cible=Page_faq">Accéder à la F.A.Q</a></li>
+        <p>La F.A.Q contient les réponses aux questions les plus communes à propos de Loghome. Si vous ne pouvait pas trouver votre question, envoyez nous une requête. Nous vous répondrons dans les plus brefs délais. </p>
+      </div>
+      <div class="contact"
+        <a href="../index.php?cible=Page_contact_us">Envoyer une requête</a></li> 
+        <p>Avez vous regardez la F.A.Q (Foire Aux Questions)? 
+</br>Cela pourrait vous aider à résoudre votre problème. Si cela n'a toujours pas résolu votre problème, contactez nous en soumettant une requête.</p>
+      </div>
     </div>
   
   </body>
