ed640c7a25d7d1e4b297a037f4ef685ba396ee19
Alban Trémenbert <alban.tremenbert@isep.fr>
2018-05-28 15:04:11 +0200

    rajout types de problèmes sur page contact us

--- vue/contact_us.php
+++ vue/contact_us.php
@@ -14,40 +14,40 @@
         <form action="../modele/contact_us_post.php" method="post">
         <div class="grid">
                     <label for="type">Type de problème :</label>
-                
-                    <select name="problem_type">
-                        <option value="type1">Type 1</option>
-                        <option value="type2">Type 2</option>
-                        <option value="type3">Type 3</option>
-                        <option value="type4">Type 4</option>
-                    </select>
-                
-            
 
-            
-                
+                    <select name="problem_type">
+                        <option value="type1">Capteur défaillant</option>
+                        <option value="type2">Installation défectueuse</option>
+                        <option value="type3">Problème site</option>
+                        <option value="type4">Autre problème</option>
+                    </select>
+
+
+
+
+
                     <label for="email">Votre adresse e-mail :</label>
-               
-                
+
+
                     <input type="email" placeholder="mon_email@example.com" name="email" id="email" />
                     <p>Parlez nous de votre problème :</p>
                     <textarea name="message" rows="8" cols="50"></textarea>
-                    
-            
-                    
-               
-            
+
+
+
+
+
         </div>
-        
+
         <div class="row"> <input type="submit" value="Envoyer" /></div>
-        
+
 
 
         </form>
-        
-        
-     
+
+
+
     </div>
     </body>
-    
+
 </html>
