5d8596aab6dec42cb3b849a09f034d83c94d8ffc
svenn <mael.tissot@isep.fr>
2018-05-29 11:17:40 +0200

    petite regulations au niveau sécurité pour l'inscription

--- controleur/CreerCompte_Erreur.php
+++ controleur/CreerCompte_Erreur.php
@@ -7,7 +7,7 @@ $Direction='../controleur/Redirection_creation.php';
 include('../modele/bdd_access.php');
 $bdd=appel_bdd();
 
-include("../modele/bdd_access_maison.php")
+include("../modele/bdd_access_maison.php");
 include('../vue/frequent/menu.php');
 include('../vue/Ajout.php');
 include('../vue/frequent/footer.php');
