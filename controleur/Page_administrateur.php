7de2f368127493e3dd5a80a268bf985bd2117156
svenn <mael.tissot@isep.fr>
2018-05-29 11:38:52 +0200

    petites modifs page admin

--- controleur/Page_administrateur.php
+++ controleur/Page_administrateur.php
@@ -57,10 +57,8 @@ body {font-family: Arial, Helvetica, sans-serif;}
 
 <!-- Trigger/Open The Modal -->
 <button id="myBtn">Open Modal</button>
-
 <!-- The Modal -->
 <div id="myModal" class="modal">
-
   <!-- Modal content -->
   <div class="modal-content">
     <span class="close">&times;</span>
@@ -72,23 +70,18 @@ body {font-family: Arial, Helvetica, sans-serif;}
 <script>
 // Get the modal
 var modal = document.getElementById('myModal');
-
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
-
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
 // When the user clicks the button, open the modal
 btn.onclick = function() {
     modal.style.display = "block";
 }
-
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
 }
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
@@ -100,17 +93,13 @@ window.onclick = function(event) {
 </body>
 </html>
 
-
-
-
 <?php
-
 function Trouver_users($utilisateurs){
      while($user=$utilisateurs->fetch()){
        if($user['ID']!='0'){
          echo '<div class="salon"><h2> ID User :'. $user["ID"].'</h2><a href="../modele/Supprimer_utilisateur.php?cible='.$user['ID'].'" class="supprimer" id="delete-button'.$user['ID'].'" onclick="return confirm(\'are you sure you want to delete this user?\');">
          Supprimer </a>
-         <a href="../controleur/Modifier_utilisateur.php?cible="'.$user["ID"].'" class="modifier" id="modifier">Modifer</a>';}}}
+         <a href="../controleur/Modifier_utilisateur.php?cible='.$user["ID"].' class="modifier" id="modifier">Modifer</a>';}}}
 
 
 ?>
