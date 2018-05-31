709db296f5b661adf5fe6523026c14335c0c28c7
Alban Trémenbert <alban.tremenbert@isep.fr>
2018-05-31 12:03:29 +0200

    non c'était celle ci oups

--- modele/bdd_access_capteur.php
+++ modele/bdd_access_capteur.php
@@ -30,25 +30,4 @@ while ($donnees = $reponse->fetch())
 }
 
 $reponse->closeCursor(); // Termine le traitement de la requête
-
-$nom=$_POST['namesensor'];
-$type=$_POST['sensortype'];
-$reference=$_POST['reference'];
-
-//$id_user=$_SESSION['id_user'];
-$ajout = $bdd->prepare('INSERT INTO capteur(ID,ID_maison,Num_Serie,Valeur,Nom,Type) VALUES(:ID,:ID_maison,:Num_Serie,:Valeur,:SNom,:Type)');
-// Requête d'insertion,
-$ajout->execute(array(
-  'ID' => $_SESSION['id_user'],
-  'ID_maison' => 4,
-  'Num_Serie' => $reference,
-  'Valeur' => 5,
-  'Nom' => $nom,
-  'Type' => $type,
-));
-
-
-header('Location: ../index.php?cible=Page_capteurs');
-?>
-
 ?>
