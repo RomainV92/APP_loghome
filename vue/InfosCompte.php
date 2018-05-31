a45cb5d9ae31119d1ac3a516291004cca23601d7
Romain Valaix <romain.valaix@isep.fr>
2018-05-31 11:07:09 +0200

    Ajout regex

--- vue/InfosCompte.php
+++ vue/InfosCompte.php
@@ -13,15 +13,18 @@
       <div class="infobox">
         <table>
       <?php
-        $infos = array('Prenom', 'Nom', 'Mail', 'Telephone');
+        $infos = array('Prénom', 'Nom', 'Mail', 'Téléphone');
         foreach($infos as $element)
         {
-          //Mise accent dans liste, mais ajout regex pour les enlever pour appel bdd
-          echo "<tr><td class=\"gauche\">".$element." :</td><td class=\"centre\">".$data[$element]."</td>";
-          if($element=="Mail" OR $element=="Telephone")
+          $sansAccent=preg_replace("#é+#", "e", $element);
+          //echo $sansAccent;
+
+          echo "<tr><td class=\"gauche\">".$element." :</td><td class=\"centre\">".$data[$sansAccent]."</td>";
+          if($element=="Mail" OR $element=="Téléphone")
             echo "<td class=\"droite\"><a class=\"bouton\" href=\"../index.php?cible=changementInfos&info=".$element."\">Modifier</a></td></tr>";
           else
             echo "<td class=\"droite\">        </td></tr>";
+
         }
       ?>
         </table>
