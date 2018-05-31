585c6d37bc1d8282f2b7e7689b5886fbb86275db
Romain Valaix <romain.valaix@isep.fr>
2018-05-31 11:52:29 +0200

    Modifs mineures

--- vue/InfosCompte.php
+++ vue/InfosCompte.php
@@ -16,9 +16,10 @@
         $infos = array('Prénom', 'Nom', 'Mail', 'Téléphone');
         foreach($infos as $element)
         {
-          echo "<tr><td class=\"gauche\">".$element." :</td><td class=\"centre\">".$data[preg_replace("#é+#", "e", $element)]."</td>";
+          $sansAccent = preg_replace("#é+#", "e", $element);
+          echo "<tr><td class=\"gauche\">".$element." :</td><td class=\"centre\">".$data[$sansAccent]."</td>";
           if($element=="Mail" OR $element=="Téléphone")
-            echo "<td class=\"droite\"><a class=\"bouton\" href=\"../index.php?cible=changementInfos&info=".$element."\">Modifier</a></td></tr>";
+            echo "<td class=\"droite\"><a class=\"bouton\" href=\"../index.php?cible=changementInfos&info=".$sansAccent."\">Modifier</a></td></tr>";
           else
             echo "<td class=\"droite\">        </td></tr>";
         }
