19abd0e331ea0cbd4e16a1db63ccf1cc4f6e4257
Romain Valaix <romain.valaix@isep.fr>
2018-05-31 11:14:23 +0200

    Ajout regex

--- vue/InfosCompte.php
+++ vue/InfosCompte.php
@@ -16,15 +16,11 @@
         $infos = array('Prénom', 'Nom', 'Mail', 'Téléphone');
         foreach($infos as $element)
         {
-          $sansAccent=preg_replace("#é+#", "e", $element);
-          //echo $sansAccent;
-
-          echo "<tr><td class=\"gauche\">".$element." :</td><td class=\"centre\">".$data[$sansAccent]."</td>";
+          echo "<tr><td class=\"gauche\">".$element." :</td><td class=\"centre\">".$data[preg_replace("#é+#", "e", $element)]."</td>";
           if($element=="Mail" OR $element=="Téléphone")
             echo "<td class=\"droite\"><a class=\"bouton\" href=\"../index.php?cible=changementInfos&info=".$element."\">Modifier</a></td></tr>";
           else
             echo "<td class=\"droite\">        </td></tr>";
-
         }
       ?>
         </table>
