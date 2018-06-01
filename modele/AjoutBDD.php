16a93546f2f34047706551716f818c8697c63954
Romain Valaix <romain.valaix@isep.fr>
2018-06-01 16:06:43 +0200

    INDENTATION

--- modele/AjoutBDD.php
+++ modele/AjoutBDD.php
@@ -8,24 +8,24 @@ try {
 }
 
 function Ajout($bdd){
-$password = $_POST['Password'];
+  $password = $_POST['Password'];
 
 
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
-if(preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $_POST['Mail']) && preg_match("#^0[1-68]([-. ]?[0-9]{2}){4}$#", $_POST['Telephone']))
-{
-$ajout = $bdd->prepare('INSERT INTO login(Nom,Prenom,Pseudo,Password,Telephone,Mail,Question,Answer) VALUES(:Nom,:Prenom,:Pseudo,:Password,:Telephone,:Mail,:Question,:Answer)');
-$ajout->execute(array(
-  'Nom' => $_POST['Nom'],
-  'Prenom' => $_POST['Prenom'],
-  'Pseudo' => $_POST['Pseudo'],
-  'Password' => $hashed_password,
-  'Telephone' => $_POST['Telephone'],
-  'Mail' => $_POST['Mail'],
-  'Question' => $_POST['Question'],
-  'Answer' => $_POST['Answer'],
-));
-return 1;
-}
-return 0;
+  $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+  if(preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $_POST['Mail']) && preg_match("#^0[1-68]([-. ]?[0-9]{2}){4}$#", $_POST['Telephone']))
+  {
+    $ajout = $bdd->prepare('INSERT INTO login(Nom,Prenom,Pseudo,Password,Telephone,Mail,Question,Answer) VALUES(:Nom,:Prenom,:Pseudo,:Password,:Telephone,:Mail,:Question,:Answer)');
+    $ajout->execute(array(
+      'Nom' => $_POST['Nom'],
+      'Prenom' => $_POST['Prenom'],
+      'Pseudo' => $_POST['Pseudo'],
+      'Password' => $hashed_password,
+      'Telephone' => $_POST['Telephone'],
+      'Mail' => $_POST['Mail'],
+      'Question' => $_POST['Question'],
+      'Answer' => $_POST['Answer'],
+    ));
+    return 1;
+  }
+  return 0;
 }
