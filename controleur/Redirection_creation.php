d0a237cd92cfa355821e3dea3149887a8df1745d
Romain Valaix <romain.valaix@isep.fr>
2018-06-01 15:54:29 +0200

    More merge conflicts

--- controleur/Redirection_creation.php
+++ controleur/Redirection_creation.php
@@ -12,17 +12,11 @@ if ($direction==1)
   Merci d'avoir créé un compte chez Loghome! \n
   Vous trouverez ci-dessous votre identifiant et un lien pour vous connecter :\n
   Votre identifiant :".$_POST['Pseudo']."\n Accéder à votre compte maintenant :https://www.loghome.fr/";
-<<<<<<< HEAD
 
   header('Location:../index.php?cible=Page_Confirmed');
   /*
   if (mail($to,$subject,$txt))
   {
-=======
-
-
-  if (mail($to,$subject,$txt)){
->>>>>>> e1458851923883d026db90770fcf2536dccd52cd
     header('Location:../index.php?cible=Page_Confirmed');
   }
   */
@@ -33,8 +27,4 @@ else
   header('Location:../index.php?cible=CreerCompte_Erreur');
 }
 
-<<<<<<< HEAD
 include('../vue/frequent/footer.php');
-=======
-?>
->>>>>>> e1458851923883d026db90770fcf2536dccd52cd
