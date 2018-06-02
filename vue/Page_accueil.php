7b49a0fb2f558d8e2d09bd262e515e32980a6bf0
jonathan <phung.truong@isep.fr>
2018-06-02 02:10:25 +0200

    changement css page d'accueil

--- vue/Page_accueil.php
+++ vue/Page_accueil.php
@@ -7,55 +7,57 @@
     <link rel="stylesheet" href="../vue/style.css" />
   </head>
 
-  <body>
+  <body  class="wrapper">
     
-    <div class="wrapper">
-      <div class="presentation">
-        <img class="picture" src="../images/equipe.jpg" alt="L'équipe" />
-        <div class="paragraphe">
+    
+      <div class="presentation1">
+        <!--<img class="picture" src="../images/equipe.jpg" alt="L'équipe" />-->
+        <div class="paragraphe1">
           <h2>Qui sommes nous?</h2>
           <p>
-          DOMISEP vous apportera la solution à tous vos tracas du quotidien :
+            DOMISEP vous apportera la solution à tous vos tracas du quotidien :
           plus besoin d'ouvrir vos volets, ni même d'allumer/éteindre la lumière,
           tout peut maintenant être automatisé grâce à notre savoir-faire.
           Nous travaillons en étroite collaboration avec Log.Home afin d'être en
           mesure de répondre à vos attentes. Essayer nos solutions, c'est les adopter!
           </p>
+          <img src="../images/our-team.jpg" alt="team">
         </div>
       </div>
 
-      <div class="presentation">
-        <div class="paragraphe1">
+      <div class="presentation2">
+        <div class="paragraphe2">
           <h2>Notre système</h2>
-          <p>Composé d'un microcontroleur à la pointe de
+          <p> Composé d'un microcontroleur à la pointe de
             la technologie, ainsi que de capteurs d'une qualité inégalée, notre système
             vous permettra d'automatiser certaines parties de votre maison
-            comme jamais auparavant. <br />
+            comme jamais auparavant. <br /><br />
                 En effet, ce système, capable de réagir à un son,
             peut être activé ou désactivé très simplement.
             Vous aurez ainsi la possibilité d'allumer des lumière ou faire tourner
             des moteurs. Sa facilité d'installation vous libérera l'esprit,
             branchez le et il sera prêt à être utilisé.</p>
           </div>
-          <img class="picture1" src="../images/architecture.jpg" alt="Notre système" />
+          <!--<img class="picture1" src="../images/architecture.jpg" alt="Notre système" />-->
         </div>
 
-      <div class="presentation">
-        <img class="picture2" src="../images/control.jpg" alt="Le panneau de contrôle" />
-        <div class="paragraphe">
+      <div class="presentation3">
+        <!--<img class="picture2" src="../images/control.jpg" alt="Le panneau de contrôle" />-->
+        <div class="paragraphe3">
+          <img src="../images/maison-connecte.jpg" alt="maison connectée">
           <h2>Contrôler vos appareils</h2>
-          <p>Une configuration de base est installée sur notre système,
+          <p> Une configuration de base est installée sur notre système,
             mais si vous souhaitez personnaliser votre appareil,
             notre panneau de contrôle, aussi bien destiné aux personnes
             peu expérimentées qu'aux spécialistes de l'informatique,
-            vous permettra de modifier vos préférences très simplement.<br />
+            vous permettra de modifier vos préférences très simplement.<br /><br />
             Vous pourrez programmer des heures auxquelles lancer
             des taches pour le CeMAC pour synchroniser votre réveil avec
             l'allumage des lumières ou encore la mise en route du moteur
             au moment où vous vous brossez les dents par exemple.</p>
           </div>
         </div>
-  </div>
+  
 
   </body>
 </html>
