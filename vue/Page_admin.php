1ac8c6bed56508af73e5894bcb67498b09ffb719
Alban Trémenbert <alban.tremenbert@isep.fr>
2018-06-08 16:47:06 +0200

    css encore en construction

--- vue/Page_admin.php
+++ vue/Page_admin.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
   <head>
@@ -36,7 +35,7 @@
   <button name="capteur" id="capteurs">Types de capteurs</button></br>
 <div id="form_capteur">
   <?php All_capteurs($type_capteurs)?>
-<<<<<<< HEAD
+
 </br>
   <div class="ajout_capteur">
     <form name="ajouter_capteur" method="post" action="../modele/ajouter_type_capteur.php">
@@ -55,24 +54,6 @@
   </div>
 
   <button name="reduire" id="reduire"> Fermer la fenêtre des capteurs</button></br>
-=======
-  <form name="ajouter_capteur" method="post" action="../modele/ajouter_type_capteur.php" enctype="multipart/form-data">
-    <label>Ajouter un nouveau type de capteur</label>
-      <p><label>Entrer le numero type du capteur: </label><input type="text" name="type_capteur" id="type" required></p>
-      <p><label>Nom de votre nouveau capteur: <input type="text" name="Nom_capteur" id="Nom" required></p>
-      <p><label>Quelle est l'unité a employé pour votre capteur?</label> <input type="text" name="AxeX" id="AxeX" required></p>
-      <p><label>En fonction de quelle unité? (nous recommandons le temps) </label><input type="text" name="AxeY" id="AxeY" required></p>
-      <p><label>veuillez rentrer une photo pour le capteur</label><input type="hidden" name="size" value="1000000">
-              <input type="file" name="image" required>
-                <button type="submit" name="upload" id="Nouveau_type">Ajouter nouveau type de capteur</button>
-          </form>
-          <div>
-
-
-          </div>
-        </form>
-        <button name="reduire" id="reduire"> Fermer fenetre des capteurs</button>
->>>>>>> 4011d57f341005d6b0b1b7cba8417918dcc584f6
       </div>
 
 
@@ -172,7 +153,7 @@ capteurs.onclick = function(){
   </body>
   </html>
 </br>
-  <a id="delete-button" href="../index.php?cible=deconnexion">Disconnect</a>
+  <a id="delete-button" href="../index.php?cible=deconnexion">Déconnexion</a>
 </br>
 </br>
   <button id="voir_util">Voir utilisateurs</button>
