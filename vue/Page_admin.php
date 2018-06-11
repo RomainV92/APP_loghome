0ce6d7faab030624d4afce955d26af832400bc6a
Alban Trémenbert <alban.tremenbert@isep.fr>
2018-06-11 14:45:34 +0200

    rajout graphe en cours

--- vue/Page_admin.php
+++ vue/Page_admin.php
@@ -8,6 +8,9 @@
 
 
   <body>
+
+
+
     <h1>Administrateur</h1><img src="../images/Logo" alt="logo" class="logo">
   <div id="content">
     <h3>Ajouter une image</h3>
@@ -184,6 +187,46 @@ voir.onclick = function(){
  }
 </script>
 
+<!-- Script pour affichage graphique -->
+<canvas id = "schema" height="181" width="300" style="border:1px solid">
+ Votre navigateur ne supporte pas la balise canvas
+</canvas>
+<script>
+var zone_dessin = document.getElementById("schema");
+var graphe= zone_dessin.getContext("2d");
+var compteur=0;
+graphe.strokeStyle = "#0098f8";
+graphe.lineWidth=3;
+graphe.beginPath();
+  graphe.moveTo(0,f(0));
+  while(compteur<10) {
+    graphe.lineTo(30*(compteur-(0)),181-(f(compteur)-(-1))*90.5);
+    compteur=(compteur+0.05);
+  }
+graphe.stroke();
+function f(x) {
+  var y=Math.sin(x);
+  return (y);
+}
+graphe.beginPath();
+  graphe.lineWidth="1";
+  graphe.strokeStyle="black";
+  graphe.moveTo(0,zone_dessin.height/2);
+  graphe.lineTo(zone_dessin.width,zone_dessin.height/2);
+  graphe.lineTo(zone_dessin.width-5,(zone_dessin.height/2)-5);
+  graphe.moveTo(zone_dessin.width,zone_dessin.height/2);
+  graphe.lineTo(zone_dessin.width-5,(zone_dessin.height/2)+5);
+  graphe.moveTo(zone_dessin.width/2,zone_dessin.height);
+  graphe.lineTo(zone_dessin.width/2,0);
+  graphe.lineTo((zone_dessin.width/2)-5,5);
+  graphe.moveTo(zone_dessin.width/2,0);
+  graphe.lineTo((zone_dessin.width/2)+5,5);
+graphe.stroke();
+graphe.fillText("0",0,10+zone_dessin.height/2);
+graphe.fillText("10",zone_dessin.width-20,10+zone_dessin.height/2);
+graphe.fillText("-1",5+zone_dessin.width/2,-8+zone_dessin.height);
+graphe.fillText("1",5+zone_dessin.width/2,8);
+</script>
 
 </body>
 </html>
