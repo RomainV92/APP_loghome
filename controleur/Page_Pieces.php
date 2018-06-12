275a9156882802dae3366f54e41ced48b0dda900
jonathan <phung.truong@isep.fr>
2018-06-12 12:48:20 +0200

    redirection lorsqu'un utilisateur veut aller dans la maison/piece d'un autre utilisateur

--- controleur/Page_Pieces.php
+++ controleur/Page_Pieces.php
@@ -7,6 +7,8 @@ $bdd=appel_bdd();
 
 include('../modele/bdd_access_maison.php');
 include('../modele/redirection_si_deco.php');
+include('../modele/redirection_si_mauvais_utilisateur_maison.php');
+
 
 include('../vue/frequent/menu.php');
 include('../vue/Pieces.php');
